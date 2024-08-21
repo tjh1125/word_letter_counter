7c36d6fd1ac623aff7ad2d6a26da9d2280f6c6d2
tjh1125 <tjh1125@hotmail.com>
2024-08-21 15:56:19 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import WordLetterCounter from './WordLetterCounter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 id='top'>Just For Funs</h1>
+      <h1>Words and Letters Counter</h1>
+      <WordLetterCounter />
+    </div>
+  );
+}
+
+export default App;
